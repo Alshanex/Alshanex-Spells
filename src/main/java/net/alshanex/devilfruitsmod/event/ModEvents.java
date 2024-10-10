464dc0c297ce565b83d8772f8794138fa72b50f9
Alshanex <discordpermacc02@gmail.com>
2024-10-10 13:49:22 +0200

    Initial Devil Fruits commit

--- src/main/java/net/alshanex/devilfruitsmod/event/ModEvents.java
+++ src/main/java/net/alshanex/devilfruitsmod/event/ModEvents.java
@@ -4,7 +4,6 @@ import net.alshanex.devilfruitsmod.DevilFruitsMod;
 import net.minecraftforge.fml.common.Mod;
 
 public class ModEvents {
-
     @Mod.EventBusSubscriber(modid = DevilFruitsMod.MOD_ID)
     public static class ForgeEvents {
 
